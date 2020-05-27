071b3ed532fc830de831b3dc8d32da9e6b371bd1
HR1Aventum <rohan.arya237@gmail.com>
2020-05-27 08:50:28 +0530

    ~first_comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactivedemo';
+  Details:FormGroup
+  constructor(){
+  this.Details = new FormGroup({
+    'name' : new FormControl(''),
+    'email' : new FormControl(''),
+    'Phone' : new FormControl(''),
+    'password' : new FormControl(''),
+    'country' : new FormControl(''),
+    'state' : new FormControl(''),
+    'city' : new FormControl(''),
+    'Gender' : new FormControl(''),
+    'Marital-status' : new FormControl(''),
+    'Fav-Food' : new FormControl(''),
+    'Fav-color' : new FormControl(''),
+    
+    
+  });
+}
+submitData(){
+  console.log(this.Details.value)
+}
 }
